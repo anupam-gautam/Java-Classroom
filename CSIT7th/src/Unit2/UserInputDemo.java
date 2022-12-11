e4efc96e44c9d1bd158a5ff0f44567ffc6fd75b2
Anupam Gautam <62124441+anupam-gautam@users.noreply.github.com>
2022-12-11 06:45:38 +0545

    First Classroom Project Practical

---
+++ CSIT7th/src/Unit2/UserInputDemo.java
@@ -0,0 +1,46 @@
+
+package Unit2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class UserInput extends JFrame{//for window
+    JLabel l1,l2,l3;
+    JTextField t1,t2;
+    JButton b1;
+    public void setComponent(){
+        l1 = new JLabel("Enter first number");
+        l2 = new JLabel("Enter second number");
+        l3 = new JLabel("Result");
+        t1 = new JTextField(30);
+        t2 = new JTextField(30);
+        b1 = new JButton("Add");
+        //adding component in window
+        add(l1);
+        add(t1);
+        add(l2);
+        add(t2);
+        add(b1);
+        add(l3);
+        //handing action event
+        b1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            
+            }
+            
+            
+        });
+        //setting properties of window
+        setVisible(true);
+        setLayout(new FlowLayout());
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+public class UserInputDemo {
+    public static void main(String[] args) {
+        UserInput u1 = new UserInput();
+        u1.setComponent();
+    }
+}
