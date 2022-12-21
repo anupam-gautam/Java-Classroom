007116383eee9c21c4fe5256362b66da917bb551
Anupam Gautam <62124441+anupam-gautam@users.noreply.github.com>
2022-12-21 06:59:36 +0545

    Update WindowEventDemo.java

--- CSIT7th/src/Unit2/WindowEventDemo.java
+++ CSIT7th/src/Unit2/WindowEventDemo.java
@@ -1,11 +1,37 @@
-
 package Unit2;
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-class WindowExample extends JFrame{
+
+class WindowEx extends JFrame{
+    JTextField t1,t2;
+    JLabel l1;
+    
     public void setWindow(){
-        //handling window 
+        t1 = new JTextField(20);
+        t2 = new JTextField(20);
+        l1 = new JLabel("result for focus");
+        
+        add(t1);
+        add(t2);
+        add(l1);        
+        
+        t1.addFocusListener(new FocusListener(){
+            @Override
+            public void focusGained(FocusEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+                l1.setText("componenet gets focus");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+                l1.setText("component loses focus");
+            }
+        });
+        
         addWindowListener(new WindowListener(){
             @Override
             public void windowOpened(WindowEvent e) {
@@ -16,50 +42,50 @@ class WindowExample extends JFrame{
             @Override
             public void windowClosing(WindowEvent e) {
                 //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-                System.out.println("window closing");
+                System.out.println("window is closing");
             }
 
             @Override
             public void windowClosed(WindowEvent e) {
-               // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-                System.out.println("window closed");
+                //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
                 dispose();
-              }
+                System.out.println("window is closed");
+            }
 
             @Override
             public void windowIconified(WindowEvent e) {
                 //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-                System.out.println("window manimized");
+                System.out.println("window is minimized");
             }
 
             @Override
             public void windowDeiconified(WindowEvent e) {
                 //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-                System.out.println("window maximized");
-               }
+                System.out.println("window is maximized");
+            }
 
             @Override
             public void windowActivated(WindowEvent e) {
                 //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-                System.out.println("window activated");
+                System.out.println("window is activated");
             }
 
             @Override
             public void windowDeactivated(WindowEvent e) {
                 //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-                System.out.println("window deactivated");
+                System.out.println("window is deactivated");
             }
-            
         });
-        setVisible(true);
-        setLayout(new FlowLayout());
-        setSize(400,400);
-        setDefaultCloseOperation(3);
+         setVisible(true);
+         setSize(200,200);
+         setLayout(new FlowLayout());
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
-public class WindowEventDemo {
+
+public class WindowEventDemo1 {
     public static void main(String[] args) {
-        WindowExample w1 = new WindowExample();
-        w1.setWindow();
+        WindowEx w = new WindowEx();
+        w.setWindow();
     }
 }
